03fe1533a6cd96be5abb7159e90e250736d6a6df
happycode2708 <happycode2708@gmail.com>
2024-05-15 01:20:42 +0700

    update

--- src/components/result/Result.tsx
+++ src/components/result/Result.tsx
@@ -27,9 +27,11 @@ const TableResult = ({ productInfo }: { productInfo: any }) => {
       {productInfo ? (
         <div className='p-4 border rounded-md flex-1 overflow-auto max-h-[500px]'>
           <MetaInfo productInfo={productInfo} />
-          {productInfo?.factPanels?.map((labelData: any, idx: number) => {
-            return <NutritionTable data={labelData} key={idx} />;
-          })}
+          <SectionWrapper name='Nutrition Fact/Supplement Fact'>
+            {productInfo?.factPanels?.map((labelData: any, idx: number) => {
+              return <NutritionTable data={labelData} key={idx} />;
+            })}
+          </SectionWrapper>
         </div>
       ) : null}
     </>
@@ -50,27 +52,26 @@ const MetaInfo = ({ productInfo }: { productInfo: any }) => {
     ocrText,
     isFactPanelLooked,
     ingredientsGroup,
-    contain,
     factPanelDebug,
     isFactPanelGoodToRead,
+    attributes,
+    allergen,
+    supplyChain,
+    header,
+    marketingAll,
+    physical,
     ...metaInfo
   } = productInfo;
 
-  const camelCaseToSeparated = (name: string) => {
-    // Split the camelCase string based on capital letters
-    const words = name.match(/[A-Z]?[a-z]+|[A-Z]+|[0-9]+/g);
-
-    // Capitalize the first letter of each word and join them with spaces
-    const separatedName = words
-      ?.map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-      .join(' ');
-
-    return separatedName;
-  };
+  const { containOnEquipment, contain } = allergen || {};
+  const { claims } = attributes || {};
+  const { marketingContents, socialMedia, ...marketingRest } =
+    marketingAll || {};
+  const { primarySize, secondarySize, ...headerRest } = header || {};
 
   return (
     <>
-      {Object.entries(metaInfo)?.map(
+      {/* {Object.entries(metaInfo)?.map(
         ([key, value]: [key: string, value: any]) => {
           if (!value) return null;
 
@@ -79,25 +80,127 @@ const MetaInfo = ({ productInfo }: { productInfo: any }) => {
               <span className='font-bold'>
                 {camelCaseToSeparated(key) ?? 'N/A'}:
               </span>
-              <span>{value}</span>
+              <span>{Array.isArray(value) ? value.join(', ') : value}</span>
+            </div>
+          );
+        }
+      )} */}
+
+      <SectionWrapper name='Header'>
+        <CamelFieldStringRender objectValues={primarySize} />
+        <CamelFieldStringRender objectValues={secondarySize} />
+        <CamelFieldStringRender objectValues={headerRest} />
+      </SectionWrapper>
+
+      <SectionWrapper name='Physical'>
+        <CamelFieldStringRender objectValues={physical} />
+      </SectionWrapper>
+
+      <SectionWrapper name='Ingredients'>
+        {ingredientsGroup?.map((ingredientItem: any, idx: number) => {
+          return (
+            <div>
+              <div className='font-bold'>Ingredient No.{idx + 1}: </div>
+              <p>{ingredientItem?.ingredients} </p>
+            </div>
+          );
+        })}
+      </SectionWrapper>
+
+      <SectionWrapper name='Attributes'>
+        {claims?.length > 0 && (
+          <div>
+            <div className='font-bold'>Attributes Claims: </div>
+            {claims?.map((claimItem: any, idx: number) => {
+              return <div className='mb-[3px]'>{claimItem?.value} </div>;
+            })}
+          </div>
+        )}
+      </SectionWrapper>
+
+      <SectionWrapper name='Marketing'>
+        {marketingContents?.length > 0 && (
+          <div>
+            <div className='font-bold'>Marketing Contents: </div>
+            {marketingContents?.map((marketingClaim: string, idx: number) => {
+              return <p className='mb-[8px]'>{marketingClaim} </p>;
+            })}
+          </div>
+        )}
+        <CamelFieldStringRender objectValues={socialMedia} />
+        <CamelFieldStringRender objectValues={marketingRest} />
+      </SectionWrapper>
+
+      <SectionWrapper name='Allergen'>
+        {containOnEquipment?.statement && (
+          <div>
+            <div className='font-bold'>Contain on equipments list</div>
+            <div>{containOnEquipment?.allergenList?.join(',')}</div>
+            <div className='font-bold'>Contain on equipment statement</div>
+            <div>{containOnEquipment?.statement}</div>
+          </div>
+        )}
+        {contain && (
+          <div>
+            <div className='font-bold'>Contain: </div>
+            <p>{contain} </p>
+          </div>
+        )}
+      </SectionWrapper>
+
+      <SectionWrapper name='Supply Chain'>
+        <CamelFieldStringRender objectValues={supplyChain} />
+      </SectionWrapper>
+    </>
+  );
+};
+
+const SectionWrapper = ({
+  name,
+  children,
+}: {
+  name: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <div>
+      <div className='font-bold bg-blue-500 p-2 rounded-md text-white'>
+        {name}
+      </div>
+      <div className='pl-8'>{children && children}</div>
+    </div>
+  );
+};
+
+const CamelFieldStringRender = ({ objectValues }: { objectValues: Object }) => {
+  return (
+    <>
+      {Object.entries(objectValues)?.map(
+        ([key, value]: [key: string, value: any]) => {
+          if (!value) return null;
+
+          return (
+            <div key={key}>
+              <span className='font-bold'>
+                {camelCaseToSeparated(key) ?? 'N/A'}:
+              </span>
+              <span>{Array.isArray(value) ? value.join(', ') : value}</span>
             </div>
           );
         }
       )}
-      {ingredientsGroup?.map((ingredientItem: any, idx: number) => {
-        return (
-          <div>
-            <div className='font-bold'>Ingredient No.{idx + 1}: </div>
-            <p>{ingredientItem?.ingredients} </p>
-          </div>
-        );
-      })}
-      {contain && (
-        <div>
-          <div className='font-bold'>Contain: </div>
-          <p>{contain} </p>
-        </div>
-      )}
     </>
   );
+};
+
+const camelCaseToSeparated = (name: string) => {
+  // Split the camelCase string based on capital letters
+  const words = name.match(/[A-Z]?[a-z]+|[A-Z]+|[0-9]+/g);
+
+  // Capitalize the first letter of each word and join them with spaces
+  const separatedName = words
+    ?.map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+
+  return separatedName;
 };
