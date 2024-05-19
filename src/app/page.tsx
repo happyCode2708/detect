11dbccd37cb3e136863db6b8f9b32bb16b4dd115
happycode2708 <happycode2708@gmail.com>
2024-05-19 13:27:17 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -190,7 +190,7 @@ export default function Home() {
                 <span>Proccessing</span>
               </div>
             ) : (
-              'Extract'
+              'Run'
             )}
           </Button>
           {loading && (
