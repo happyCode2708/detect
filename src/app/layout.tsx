0e66181302e69a8f3f95a931ffa9e510bb0f5eb9
happycode2708 <happycode2708@gmail.com>
2024-07-05 02:22:48 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import Provider from '@/provider/Provider';
 import { Toaster } from '@/components/ui/toaster';
-import { server_useUserInfo } from '@/hooks/user';
 
 const inter = Inter({ subsets: ['latin'] });
 
